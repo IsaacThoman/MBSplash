dc48d90e198db5c9b0bfbbebd95e17c79a6411c4
isaacthoman <isaacthoman@outlook.com>
2023-05-20 18:09:08 -0400

    fish

--- index.js
+++ index.js
@@ -18,17 +18,37 @@ const img = new Image();
 img.src = 'image.png';
 let lastChangeTime = 0;
 
+let images = [];
+
+for(let i = 0; i < 34; i++) {
+    let img = new Image();
+    img.src = "/fish2/fish" + (i+1) + ".webp";
+    img.onload = function() {
+        console.log('Image loaded: ' + this.src);
+    };
+    img.onerror = function() {
+        console.log('Error loading image: ' + this.src);
+    };
+    images.push(img);
+}
+
+let frameOn = 0;
 function doFrame(){
+    frameOn++;
    // ctx.fillStyle = '#cc6600';
-    if(myId>0){
+    if(myId>0.1){
         ctx.fillStyle = '#000000';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         let freeYSpace = canvas.width*9/16 - canvas.height;
         ctx.drawImage(img, 0, -freeYSpace/2, canvas.width, canvas.width*9/16);
     }
     else{
-        ctx.fillStyle = '#cc6600';
+        ctx.fillStyle = '#000000';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
+        let freeYSpace = canvas.width*9/16 - canvas.height;
+
+        ctx.drawImage(images[(frameOn)%34], 0, -freeYSpace/2, canvas.width, canvas.width*9/16);
+
     }
 
     let currentTimeUTC = Math.round(new Date().getTime()/1000);
